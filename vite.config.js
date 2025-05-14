bf6bec66e6fbd1da2defd7075625406fc9c9dc5a
BHurt <brandonhurt208@gmail.com>
2025-05-14 17:12:09 -0500

    final changes

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "https://brimbis.github.io/src/main.jsx",
+  base: "/brimbis.github.io/", 
 })
